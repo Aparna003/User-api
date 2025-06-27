07da5af6c8f538f4db677150d42065bd4ccacf5e
Aparna003 <aparnashankar004@gmail.com>
2025-06-27 17:30:00 -0400

    JWT Utility and Login controller

---
+++ src/main/java/com/example/user_api/dto/AuthController.java
@@ -0,0 +1,43 @@
+package com.example.user_api.dto;
+
+import com.example.user_api.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/authenticate")
+    public AuthResponse createToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authRequest.getUsername(),
+                            authRequest.getPassword()
+                    )
+            );
+        }catch (BadCredentialsException e){
+            throw new Exception(" Incorrect username or password",e);
+    }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+        return new AuthResponse(jwt);
+    }
+}
