832389f5309dc02b75ff58fce34f91204ba457bf
Aparna003 <aparnashankar004@gmail.com>
2025-07-22 19:49:58 -0400

    Postgres setup in progress

--- src/main/java/com/example/user_api/UserRepository.java
+++ src/main/java/com/example/user_api/UserRepository.java
@@ -1,6 +1,9 @@
 package com.example.user_api;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
